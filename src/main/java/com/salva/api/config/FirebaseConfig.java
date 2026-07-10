8ab0b89bcf26b4ce0d4ecb65773f994703b4059a
luanahoegen <luanahoegen123@gmail.com>
2026-07-09 21:14:52 -0300

    Remove service account key

--- src/main/java/com/salva/api/config/FirebaseConfig.java
+++ src/main/java/com/salva/api/config/FirebaseConfig.java
@@ -4,10 +4,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -15,8 +17,8 @@ public class FirebaseConfig {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            Resource resource = new ClassPathResource("serviceAccountKey.json");
+            InputStream serviceAccount = resource.getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -27,6 +29,7 @@ public class FirebaseConfig {
             }
         } catch (IOException e) {
             e.printStackTrace();
+            System.err.println("Erro ao inicializar o Firebase: " + e.getMessage());
         }
     }
 }
